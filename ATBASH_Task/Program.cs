e1583b77347ba7ed23d4c0998b73a5cad41b0155
Moti <shalom7950@gmail.com>
2025-05-14 15:03:08 +0300

    Update decrypt func

--- ATBASH_Task/Program.cs
+++ ATBASH_Task/Program.cs
@@ -8,9 +8,8 @@ namespace ATBASH_Task
 {
     internal class Program
     {
-        
-        
-
+        static void DecryptAtbadh(string input)
+        {
             Dictionary<char, char> atbashCipher = new Dictionary<char, char>
             {
                 {'A', 'Z'}, {'B', 'Y'}, {'C', 'X'}, {'D', 'W'}, {'E', 'V'},
@@ -21,7 +20,24 @@ namespace ATBASH_Task
                 {'Z', 'A'}
             };
 
-        
+            List<string> dicript = new List<string>();
+            foreach (char c in input)
+            {
+                if (atbashCipher.ContainsKey(c))
+                {
+                    dicript.Add(atbashCipher[c].ToString());
+                }
+                else if (char.IsWhiteSpace(c))
+                {
+                    dicript.Add(" ");
+                }
+                else
+                {
+                    Console.WriteLine("Not in ATBASH Dicti!");
+                }
+                    
+            }
+        }
 
         static void Main(string[] args)
         {
